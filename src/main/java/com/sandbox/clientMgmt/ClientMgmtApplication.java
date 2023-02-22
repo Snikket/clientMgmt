7c32be6d2e5ac24af104d0065fef99dfde3d8822
Mamas Nicolaou <MAMASN@amdocs.com>
2023-02-22 17:36:01 +0200

    initial commit

---
+++ src/main/java/com/sandbox/clientMgmt/ClientMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sandbox.clientMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientMgmtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sandbox/clientMgmt/command/RegisterClientCommand.java
@@ -0,0 +1,8 @@
+package com.sandbox.clientMgmt.command;
+
+public class RegisterClientCommand {
+    private String clientId;
+    private String referalCode;
+    private String landingPage;
+
+}
